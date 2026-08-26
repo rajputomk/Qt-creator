b9cd3b2df1e965f4d0a23c6a10b2f51a00fb32c2
Rajput Om <rajputom0101@gmail.com>
2026-08-26 14:01:21 +0530

    Add files via upload

---
+++ day 2/1/build/Desktop-Debug/CMakeFiles/1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "1_autogen"
+  "CMakeFiles/1.dir/link.d"
+  "CMakeFiles/1_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/1_autogen.dir/ParseCache.txt"
+  "1"
+  "1.pdb"
+  "1_autogen/mocs_compilation.cpp"
+  "1_autogen/timestamp"
+  "CMakeFiles/1.dir/1_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/1.dir/1_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/1.dir/main.cpp.o"
+  "CMakeFiles/1.dir/main.cpp.o.d"
+  "CMakeFiles/1.dir/mainwindow.cpp.o"
+  "CMakeFiles/1.dir/mainwindow.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ day 2/1/build/Desktop-Debug/CMakeFiles/1_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1_autogen/include/ui_mainwindow.h"
+  "1_autogen/mocs_compilation.cpp"
+  "1_autogen/timestamp"
+  "CMakeFiles/1_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/1_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
